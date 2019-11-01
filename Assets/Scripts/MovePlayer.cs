f872b048ad731ca7cd33a22d543225535447a049
Kachan Ilya <BioKotik@github.com>
2019-11-01 10:56:07 +0300

    GameZeldaLike

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public enum PlayerState
+    {
+        walk,
+        attack,
+        interact
+    }
+
+    public PlayerState currentState;
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector3 change;
+    private Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentState = PlayerState.walk;
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if(Input.GetButtonDown("Attack") && currentState != PlayerState.attack)
+        {
+            StartCoroutine(AttackCo());
+        }
+        else if (currentState == PlayerState.walk)
+        {
+            UpdateAnimationAndMove();
+        }
+        
+    }
+
+    private IEnumerator AttackCo()
+    {
+        animator.SetBool("Attacking", true);
+        currentState = PlayerState.attack;
+        yield return null;
+        animator.SetBool("Attacking", false);
+        yield return new WaitForSecondsRealtime(.3f);
+        currentState = PlayerState.walk;
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("MoveX", change.x);
+            animator.SetFloat("MoveY", change.y);
+            animator.SetBool("Moving", true);
+        }
+        else
+        {
+            animator.SetBool("Moving", false);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        rb.MovePosition(transform.position + change * speed * Time.deltaTime);
+    }
+}
